a35fbb01733e723daec8d254441c6244d2203e7c
xhlar <mauricio@xhlar.com>
2023-09-09 12:54:07 -0500

    folders and script
    Signed-off-by: xhlar <mauricio@xhlar.com>

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Project: UseFul Apps
+# Author: CompilerByte
+# Date: 2023-09-09
+# Version: 1.0.0
+# Description: Install all the apps in the system using this script
+# Usage: ./install.sh
+# Note: Run this script as root
