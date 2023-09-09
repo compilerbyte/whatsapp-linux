69b4b6b4fd3a6f9be319c7ade4caba6dfc92c86d
xhlar <mauricio@xhlar.com>
2023-09-09 12:24:54 -0500

    adding launcher
    Signed-off-by: xhlar <mauricio@xhlar.com>

---
+++ whatsapp.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+# Description: This script is used to open whatsapp like app on desktop using brave browser 
+# Author: CompilerByte
+# Date: 2023-09-09
+# Version: 1.0
+brave-browser --app=https://web.whatsapp.com &
